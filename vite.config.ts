022ad7b7764e31dd396f2008170cebb3e5a491f1
flow145 <268885866+flow145@users.noreply.github.com>
2026-08-25 20:09:36 +0500

    chore: set up svg icons

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,24 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
 
-  plugins: [react(), mkcert()],
+  plugins: [
+    react(),
+    mkcert(),
+    svgr({
+      include: 'src/shared/assets/**/*.svg',
+      svgrOptions: {
+        ref: true,
+        titleProp: true,
+      },
+    }),
+  ],
 
   build: {
     target: ['chrome111', 'edge111', 'firefox114', 'safari16.4', 'ios16.4'],
