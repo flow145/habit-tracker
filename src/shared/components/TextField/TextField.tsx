007fac1f5e1ce18e7ec7ff42579e017cde8dd6b4
flow145 <268885866+flow145@users.noreply.github.com>
2026-08-26 18:21:46 +0500

    refactor: support textarea fields in TextField

--- src/shared/components/TextField/TextField.tsx
+++ src/shared/components/TextField/TextField.tsx
@@ -3,15 +3,26 @@ import { clsx } from 'clsx'
 
 import styles from './TextField.module.css'
 
+export type TextFieldComponent = 'input' | 'textarea'
+
 export interface TextFieldProps
   extends Pick<FieldRootProps, 'dirty' | 'touched' | 'disabled' | 'invalid' | 'className'>,
     Omit<FieldControlProps, 'className' | 'style' | 'render'> {
+  component?: TextFieldComponent
   label: string
   error?: string
   hideLabel?: boolean
 }
 
+/**
+ * - this doesn't work well with textarea types
+ * - no auto-resizing
+ * - no way to hide the default handle and keep resizing (to add a custom handle)
+ * TODO use Textarea component https://github.com/mui/base-ui/issues/718
+ */
+
 export const TextField = ({
+  component = 'input',
   label,
   error,
   hideLabel,
@@ -30,7 +41,11 @@ export const TextField = ({
         {label}
       </Field.Label>
       <div className={styles.group}>
-        <Field.Control className={clsx(styles.control, 'body')} {...controlProps} />
+        <Field.Control
+          render={component === 'textarea' ? <textarea rows={3} /> : undefined}
+          className={clsx(styles.control, 'body')}
+          {...controlProps}
+        />
         {error && (
           <Field.Error className={clsx(styles.error, 'hint')} match>
             {error}
